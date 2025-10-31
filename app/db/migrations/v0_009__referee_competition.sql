d418755bd72a9f88276eaadb0f81f6736f19b20b
mandibchaulagain <mandibchaulagain8@gmail.com>
2025-10-31 14:26:16 +0545

    name of the files changed for better versioning

--- app/db/migrations/v0_009__referee_competition.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE referee_competition (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    referee_id INT NOT NULL,
-    competition_id INT NOT NULL,
-    FOREIGN KEY (referee_id) REFERENCES referees(id) ON DELETE CASCADE,
-    FOREIGN KEY (competition_id) REFERENCES competitions(id) ON DELETE CASCADE
-);
